908eaeeef18ddae60cd46397b69fa0003be10058
mos03351 <mos03351@gmail.com>
2022-05-17 09:00:24 +0700

    book-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookComponent } from './component/add-book/add-book.component';
+import { DetailBookComponent } from './component/detail-book/detail-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddBookComponent
+    AddBookComponent,
+    DetailBookComponent
   ],
   imports: [
     BrowserModule,
